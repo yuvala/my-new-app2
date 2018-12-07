32021c989149d7df588ebecfa06c4a96a8119975
yuval almaliah <yuv.almaliah@hmail.com>
2018-12-07 14:28:39 +0200

    add more files|

---
+++ src/app/reciepes/reciepes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reciepes',
+  templateUrl: './reciepes.component.html',
+  styleUrls: ['./reciepes.component.css']
+})
+export class ReciepesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
